ff58a407e8450812762802ae618361960da2644e
Nathy Rodrigues <nathalia.r.s@outlook.com>
2022-12-26 21:47:34 -0300

    adicionando projeto modificado

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Coldplay anuncia novas datas de shows no Rio após cancelamentos",
+    "description": "A banda adiou a turnê no Brasil por conta de um problema de saúde do vocalista Chris Martin, que foi diagnosticado com uma infecção no pulmão. Depois de adiar as duas apresentações que faria no Rio de Janeiro, marcadas para os dias 11 e 12 de outubro, o Coldplay anunciou nesta segunda-feira (10/10) novas datas para os shows na Cidade Maravilhosa. Os espetáculos da banda britânica, responsável por um dos grandes momentos do Rock in Rio 2022, foram remarcadas para os dias 25 e 26 de março do ano que vem. O local permanece o mesmo, o Estádio Nilton Santos, o Engenhão, na Zona Norte da capital fluminense. Os ingressos adquiridos anteriormente serão válidos para as novas datas, não sendo necessária nenhuma ação dos portadores de ingressos. O grupo também remarcou as datas dos shows em São Paulo, que agora irão acontecer nos dias 10, 11, 13, 14, 17 e 18 de março de 2023.",
+    "photoCover":"https://www.uniaofm.com.br/wp-content/uploads/2021/12/coldplay-1493074030.58.2560x1440.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "title": "Coldplay lançará suas últimas músicas novas em 2025",
+    "description": "O Coldplay vai deixar de produzir músicas novas após 2025. A informação foi anunciada pelo vocalista do grupo, Chris Martin, em uma entrevista à âncora Jo Whiley, da emissora BBC Radio 2. “Nosso último álbum sairá em 2025 e, depois disso, acho que só faremos turnês”, disse Martin em um trailer da entrevista, divulgado no fim da noite desta quarta-feira (22).",
+    "photoCover":"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/10/2560px-Coldplay_-_Global-Citizen-Festival_Hamburg_10-e1634214512837.jpg?w=876&h=484&crop=1"
+  },
+  {
+    "id":"3",
+    "title": "Coldplay celebra um bilhão de streams de “Viva La Vida”",
+    "description": "O Coldplay está celebrando nas redes sociais o bilhão de streams da música “Viva La Vida”. No vídeo disponibilizado nesta terça-feira (29), a banda mostra um enorme cabeamento de fios até chegar na brilhante placa enviada pelo Spotify. “Viva la Vida” foi lançada em 2008 e está presente no álbum “Viva la Vida or Death and All His Friends”. A faixa chegou ao topo dos principais mercados fonográficos do mundo, o do Reino Unido e dos Estados Unidos, e levou diversos prêmios. Entre eles, o Grammy de “Música do Ano”, em 2009. ",
+    "photoCover":"https://i.ytimg.com/vi/jcu7UWeObPA/maxresdefault.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Show do Coldplay em Buenos Aires tem transmissão ao vivo em cinemas pelo mundo",
+    "description": "Depois do show de sucesso no Rock in Rio 2022, Coldplay anunciou nesta quinta-feira (15) mais uma apresentação, desta vez em Bueno Aires, no Estádio Monumental de Núñez. A novidade é que o show, realizado nos dias 28 e 29 de outubro, será transmitido ao vivo em mais de 70 cinemas do mundo. Ainda não há confirmação se os cinemas brasileiros irão receber a transmissão, mas é possível acompanhar as atualizações da ação no site oficial. No mesmo lugar, também é possível comprar ingressos para as sessões já divulgadas. O ingresso custa R$ 91,19. Para os pagantes de meia entrada, o valor é de R$ 51,19. ",
+    "photoCover":"https://www.radiorock.com.br/wp-content/uploads/2022/10/coldplay.jpg"
+  },  
+
 ]
